0e880a0c019adeaf31d633e1492f8c2270838131
LAPTOP-Q0L54CT7\Sugyan Subedi <sujit.subedi@selu.edu>
2019-05-07 20:57:53 -0500

    database

---
+++ TheInternationalHubApi/TheInternationalHubApi/Models/testdatabaseContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TheInternationalHubApi.Models
+{
+    public partial class testdatabaseContext : DbContext
+    {
+        public testdatabaseContext()
+        {
+        }
+
+        public testdatabaseContext(DbContextOptions<testdatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.;Database=testdatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.Property(e => e.CountryId)
+                    .HasColumnName("Country_ID")
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.WNumber);
+
+                entity.HasIndex(e => e.Phone)
+                    .HasName("STUDENTUNIQ")
+                    .IsUnique();
+
+                entity.Property(e => e.WNumber)
+                    .HasColumnName("W_Number")
+                    .HasMaxLength(7)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CountryId)
+                    .IsRequired()
+                    .HasColumnName("Country_ID")
+                    .HasMaxLength(3)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gpa)
+                    .HasColumnName("GPA")
+                    .HasMaxLength(3)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Major)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sex)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Country)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.CountryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("StudentCountry");
+            });
+        }
+    }
+}
